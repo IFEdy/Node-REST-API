13fe692afce71fbc9f55438e7cac4387e85f2ab9
ADEYEMI IFEDAYO O <adeyemiife@ymail.com>
2018-03-30 14:27:59 -0700

    made by me

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ var logger = require("morgan");
 app.use(logger("dev"));
 app.use(jsonParser());
 
+//ERROR handler
+app.use(function(req, res, next){
+    var err = new error("NOT FOUND");
+    err.status = 404;
+    next(err);
+});
+
 app.use("/questions", routes);
 var port = process.env.PORT || 3000;
 
--- route.js
+++ route.js
@@ -17,7 +17,8 @@ router.post("/", function(req, res){
 
 router.get("/:qID", function(req, res){
     res.json({
-        response: "There is a GET request with a specific ID " + req.params.qID
+        response: "There is a GET request with a specific ID " + req.params.qID,
+        questionID : req.params.qID
     });
 });
 
@@ -25,30 +26,40 @@ router.get("/:qID", function(req, res){
 //route for creating answwers
 router.post("/:qID/answers", function(req, res){
     res.json({response: "There is a POST request to /answers",
-    questiodID: req.params.qID,
+    questionID: req.params.qID,
     body: req.body
 });
 });
 
 
-//POST questions/:qID/answers/:id
+//POST questions/:qID/answers/:aID
 //route for editting a specific answers
 router.put("/:qID/answers/:aID", function(req, res){
-    res.json({response: "There is a POST request to /answers/aID",
-    questiodID: req.params.qID,
+    res.json({response: "There is a PUT request to /answers/aID",
+    questionID: req.params.qID,
     answerID: req.params.aID,
     body: req.body
 });
 });
 
 
-//POST questions/:qID/answers/:id
+//POST questions/:qID/answers/:aID
 //route to delete a specific answers
 router.delete("/:qID/answers/:aID", function(req, res){
-    res.json({response: "There is a POST request to /answers/aID",
-    questiodID: req.params.qID,
+    res.json({response: "There is a DELETE request to /answers/aID",
+    questionID: req.params.qID,
+    answerID: req.params.aID
+});
+});
+
+//POST questions/:qID/answers/:aID/vote-up
+//POST questions/:qID/answers/:aID/vote-down
+//route to VOTE for a specific answers
+router.post ("/:qID/answers/:aID/vote-:dir", function(req, res){
+    res.json({response: "There is a VOTE request to /answers/aID " + req.params.dir,
+    questionID: req.params.qID,
     answerID: req.params.aID,
-    body: req.body
+    vote: req.params.dir
 });
 });
 
