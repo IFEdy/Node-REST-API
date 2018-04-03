3222f968cf6e3aae66c76632761b439f8a192a9a
ADEYEMI IFEDAYO O <adeyemiife@ymail.com>
2018-04-03 16:58:35 -0700

    made by me

--- route.js
+++ route.js
@@ -55,7 +55,7 @@ router.delete("/:qID/answers/:aID", function(req, res){
 //POST questions/:qID/answers/:aID/vote-up
 //POST questions/:qID/answers/:aID/vote-down
 //route to VOTE for a specific answers
-router.post ("/:qID/answers/:aID/vote-:dir",function(req, res){
+router.post ("/:qID/answers/:aID/vote-:dir",function(req, res, next){
     if(req.params.dir.search(/^(up|down)$/) === -1){
         var err = new Error("Not found");
         err.status = 404;
