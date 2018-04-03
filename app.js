8e5444a305df93417bb9d44326ff1321c55ee392
ADEYEMI IFEDAYO O <adeyemiife@ymail.com>
2018-04-03 12:04:22 -0700

    made by me

--- app.js
+++ app.js
@@ -11,9 +11,19 @@ app.use(jsonParser());
 
 //ERROR handler
 app.use(function(req, res, next){
-    var err = new error("NOT FOUND");
+    var err = new Error("NOT FOUND");
     err.status = 404;
     next(err);
+});
+
+//Second ERROR handler
+app.use(function(err, req, res, next){
+    res.status(err.status || 500);
+    res.json({
+        error : {
+            message: err.message
+        }
+    });
 });
 
 app.use("/questions", routes);
